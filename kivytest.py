62ed129f3aaea174679a3adc5963e25f4a5b9c51
bailong8281566 <bailong8281566@gmail.com>
2020-08-31 09:22:00 +0800

    add one

--- kivytest.py
+++ kivytest.py
@@ -3,7 +3,7 @@ from kivy.uix.button import Button
 
 class TetsApp(App):
     def build(self):
-        return Button(text="Hello World!")
+        return Button(text="Hello World one!")
 
 if __name__ == "__main__":
     TetsApp().run()
